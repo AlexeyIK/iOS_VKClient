d0a8311006c171d03e6368468de53a56ce7eec7a
alexeykuznetsov <oasisalex@gmail.com>
2019-12-06 08:58:57 +0300

    Наработки для круглых аватарок

---
+++ iOS_UI_practice1/UI/AvatarView.swift
@@ -0,0 +1,33 @@
+//
+//  CustomView.swift
+//  iOS_UI_practice1
+//
+//  Created by Alex on 05.12.2019.
+//  Copyright © 2019 Alexey Kuznetsov. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable class AvatarView : UIView {
+    
+    @IBInspectable var mainColor : UIColor = UIColor.white
+    @IBInspectable var cornerRadius : CGFloat = 20 {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    @IBInspectable var shadowRadius : CGFloat = 10
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        
+        layer.cornerRadius = cornerRadius
+        layer.shadowRadius = shadowRadius;
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.1
+        setNeedsDisplay()
+    }
+}
