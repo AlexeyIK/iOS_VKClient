02b06530c3685faec7ac6cad15a932ef815d5927
alexeykuznetsov <oasisalex@gmail.com>
2019-12-16 23:44:59 +0300

    Реализован Loader после логина

---
+++ iOS_UI_practice1/UI/Loader.swift
@@ -0,0 +1,21 @@
+//
+//  Loader.swift
+//  iOS_UI_practice1
+//
+//  Created by Alex on 16.12.2019.
+//  Copyright © 2019 Alexey Kuznetsov. All rights reserved.
+//
+
+import UIKit
+
+class Loader: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
