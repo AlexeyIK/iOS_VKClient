e310ee21f9bbdf065e86c4a541ae35be42ea515d
alexeykuznetsov <oasisalex@gmail.com>
2019-12-26 09:17:55 +0300

    Задел под несколько фотографий в посте

---
+++ iOS_UI_practice1/News/Cells/MultiphotoPostCell.swift
@@ -0,0 +1,24 @@
+//
+//  MultiphotoPostCell.swift
+//  iOS_UI_practice1
+//
+//  Created by Alex on 25.12.2019.
+//  Copyright © 2019 Alexey Kuznetsov. All rights reserved.
+//
+
+import UIKit
+
+class MultiphotoPostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
